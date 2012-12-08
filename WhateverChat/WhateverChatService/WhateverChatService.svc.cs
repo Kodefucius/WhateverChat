5e7d4dbf832b2c11d6c99a6d3b1447af9df57819
Vali <vali.malinoiu@xerox.com>
2012-12-08 11:28:43 +0200

    Added azure project and the service project

---
+++ WhateverChat/WhateverChatService/WhateverChatService.svc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WhateverChatService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WhateverChatService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select WhateverChatService.svc or WhateverChatService.svc.cs at the Solution Explorer and start debugging.
+    public class WhateverChatService : IWhateverChatService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
